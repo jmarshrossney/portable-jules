357d4fe50abcb5a55494e1fa81f2d08b6e321557
jmarshrossney <17361029+jmarshrossney@users.noreply.github.com>
2025-07-09 15:13:54 +0100

    combine and improve setup+build scripts

--- devbox/scripts/setup.sh
+++ devbox/scripts/setup.sh
@@ -1,10 +1,25 @@
-# Executed every time a new devbox shell is created
-
+# Check script was executed using devbox run or in a devbox shell
 if [[ ! $DEVBOX_SHELL_ENABLED -eq 1 ]]; then
-    echo "Value of \$DEVBOX_SHELL_ENABLED is: $DEVBOX_SHELL_ENABLED"
     echo "This script must be run inside a devbox shell"
+    echo "(The value of \$DEVBOX_SHELL_ENABLED is: $DEVBOX_SHELL_ENABLED)"
     exit 1
 fi
+# Check that FCM and JULES directories are not in use already
+if [ -d "$FCM_ROOT" ]; then
+    echo "There is already a directory at $FCM_ROOT"
+    exit 1
+fi
+if [ -d "$JULES_ROOT" ]; then
+    echo "There is already a directory at $JULES_ROOT"
+    exit 1
+fi
+
+# Exit if any subsequent command returns non-zero exit status
+set -e
+
+# ------------------------------------------- #
+#  Read MOSRS credentials and JULES revision  #
+# ------------------------------------------- #
 
 # Attempt to read from env variables, otherwise take default values
 REVISION="${JULES_REVISION:-HEAD}"
@@ -34,57 +49,61 @@ while getopts "r:u:p" opt; do
     esac
 done
 
-
-# Check that credentials have been set
+# Check that MOSRS credentials have been set
 if [ "$USERNAME" = "__MISSING__" ] || [ -z "$PASSWORD" = "__MISSING__" ]; then
     echo "Must provide a username and password!"
     exit 1
 fi
 
-curr_dir=$PWD
-tmpdir=$DEVBOX_PROJECT_ROOT/tmp
+# Switch to temporary directory
+curr_dir="$PWD"
+tmpdir="$DEVBOX_PROJECT_ROOT/_tmp"
+mkdir -v "$tmpdir"
+cd "$tmpdir"
 
-mkdir -v $tmpdir
-cd $tmpdir
 
-# Download and extract fcm if it doesn't already exist
-if [ ! -d $FCM_ROOT ]; then
+# ------------------------------------------------------ #
+#  Download and extract fcm if it doesn't already exist  #
+# ------------------------------------------------------ #
 
-    # TODO: consider alternatives to hard-coded most recent version
-    fcm_version="2021.05.0"
-    fcm_release="https://github.com/metomi/fcm/archive/refs/tags/${fcm_version}.tar.gz"
+# NOTE: hard-coded most recent version, but releases are *very* infrequent
+fcm_version="2021.05.0"
+fcm_release="https://github.com/metomi/fcm/archive/refs/tags/${fcm_version}.tar.gz"
 
-    echo "Downloading and extracting $fcm_release to $FCM_ROOT"
+echo "Downloading and extracting $fcm_release to $FCM_ROOT"
+curl -L $fcm_release | tar -xz
 
-    curl -L $fcm_release | tar -xz
-    mv -v fcm-$fcm_version $FCM_ROOT
+mkdir -p -v "$(dirname $FCM_ROOT)"
+mv -v fcm-$fcm_version "$FCM_ROOT"
 
-else
-    echo "There is already a directory at $FCM_ROOT. Skipping the download..."
-fi
 
-if [ ! -d $JULES_ROOT ]; then
+# -------------------------------------------- #
+#  Download JULES if it doesn't already exist  #
+# -------------------------------------------- #
+echo "Downloading JULES, revision $REVISION"
 
-    echo "Downloading JULES"
+svn checkout --non-interactive --no-auth-cache  \
+    --username "$USERNAME" --password "$PASSWORD" \
+    https://code.metoffice.gov.uk/svn/jules/main/trunk --revision "$REVISION" \
+    jules
 
-    svn checkout --non-interactive --no-auth-cache  \
-	    --username "$USERNAME" --password "$PASSWORD" \
-	    https://code.metoffice.gov.uk/svn/jules/main/trunk --revision "$REVISION" \
-	    jules
+# NOTE: need to add the -fallow-argument-mismatch -w flags so that gfortran doesn't complain
+printf '\n# We are forced to suppress a gfortran error about non-standard code\nbuild.prop{fc.flags}[jules/src/io/dump/read_dump_mod.F90] = $fflags_common -fallow-argument-mismatch -w' >> jules/etc/fcm-make/platform/custom.cfg
 
-    # NOTE: need to add the -fallow-argument-mismatch -w flags so that gfortran doesn't complain
-    printf '\n# We are forced to suppress a gfortran error about non-standard code\nbuild.prop{fc.flags}[jules/src/io/dump/read_dump_mod.F90] = $fflags_common -fallow-argument-mismatch -w' >> jules/etc/fcm-make/platform/custom.cfg
+mkdir -p -v "$(dirname $JULES_ROOT)"
+mv -v jules "$JULES_ROOT"
 
-    mv -v jules $JULES_ROOT
 
-else
-    echo "There is already a directory at $JULES_ROOT. Skipping the download..."
-fi
+# Return to original directory and delete temporary directory
+cd "$curr_dir"
+rmdir -v "$tmpdir"
 
-cd $curr_dir
 
-rmdir -v $tmpdir
+# ------------- #
+#  Build JULES  #
+# ------------- #
+mkdir -p -v "$JULES_BUILD_DIR"
+echo "Building JULES"
+"$FCM_ROOT/bin/fcm" make -f "$JULES_ROOT/etc/fcm-make/make.cfg" -C "$JULES_BUILD_DIR" --new
 
-# Install Python packages
-#python -m pip install --upgrade pip
-#python -m pip install -r $DEVBOX_PROJECT_ROOT/requirements.txt
+echo "Setup completed successfully"
